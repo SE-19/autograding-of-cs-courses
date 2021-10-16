7800092ee6062a365e281799085626cfda49784c
Hammad-001 <hammadfaisal178@gmail.com>
2021-10-16 15:25:33 +0500

    app.py updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "Home Page here..."
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
