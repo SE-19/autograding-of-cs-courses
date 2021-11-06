83af7ff255ed40cff725983f26e6642f5824222e
nabeelraza-7 <razanabeel512@gmail.com>
2021-11-06 21:43:20 +0500

    Changed login and created database for teachers

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import *
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///autograde.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class Teacher(db.Model):
+    teacher_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    cnic = db.Column(db.String(13), unique=True, nullable=False)
+    phone_no = db.Column(db.String(11), unique=True, nullable=False)
+    department = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"{self.teacher_id}, {self.name}"
 
 @app.route("/")
 def homepage():
---
+++ database.py
@@ -0,0 +1,5 @@
+from enum import unique
+from app import db
+
+
+
