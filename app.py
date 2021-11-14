9148e46bf209c21a6a13a633302bcbfbda3159ed
Hussnain <hussnainilyas2344@gmail.com>
2021-11-14 18:08:03 +0500

    Created TA page

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, session, url_for, f
 from flask.helpers import url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
+
 from werkzeug.security import check_password_hash, generate_password_hash
 import secrets
 SECRET_KEY = secrets.token_urlsafe(16)
@@ -22,7 +23,8 @@ class Teacher(db.Model):
     password = db.Column(db.String(500), nullable=False)
     tas=db.relationship('TA',backref="head")
     assignments=db.relationship('Assignment',backref="teacher")
-    
+
+
     def __repr__(self) -> str:
         return f"{self.teacher_id}, {self.name}"
 
@@ -110,6 +112,11 @@ def login_register():
     return render_template("logreg.html")
 
 
+
+
+
+
+
 @app.route("/create_assignment")
 def create_assignment():
     return render_template("create_assignment.html")
@@ -122,9 +129,22 @@ def mark_assignment():
 def assignments():
     return render_template("assignments.html")
 
+@app.route("/TA_manage")
+def TA_manage():
+    return render_template("TA_manage.html")
+
+@app.route("/OTP")
+def OTP():
+    return render_template("OTP.html")
+
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('homepage'))
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
