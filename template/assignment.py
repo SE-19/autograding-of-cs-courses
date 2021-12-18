a261c9d5b6af349bf35c7499d4904704267b21ab
nabeelraza-7 <razanabeel512@gmail.com>
2021-12-18 11:37:18 +0500

    Final Commit

--- template/assignment.py
+++ template/assignment.py
@@ -1,6 +1,12 @@
-def jk(klajsf):
+def to_lower_case(s):
     """
-    
+    This function should return lowercase `s`
+    """
+    return None
+
+def fibonacci(nth_term):
+    """
+    give nth term in fibonacci series
     """
     return None
 
